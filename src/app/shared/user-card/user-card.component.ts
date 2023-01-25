2cf5c1b3403a01946f48a5a55a3a9e7efceb2818
Akash Raju M <akashrajum7@gmail.com>
2023-01-25 23:49:00 +0530

    user card and search fixes

--- src/app/shared/user-card/user-card.component.ts
+++ src/app/shared/user-card/user-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.css']
+  styleUrls: ['./user-card.component.css'],
 })
 export class UserCardComponent {
-
+  @Input()
+  src: string = '';
+  @Input()
+  name: string = '';
+  @Input()
+  username: string = '';
 }
