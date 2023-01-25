eb91f4e7f54aea112b4972fbf7049c9fcb6d954b
Akash Raju M <akashrajum7@gmail.com>
2023-01-25 23:13:42 +0530

    add navbar functionalities

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
+  constructor(private router: Router) {}
 
+  isMobileMenuOpen: boolean = false;
+
+  toggleMenu(event: Event) {
+    event.preventDefault();
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  get isHome() {
+    return this.router.url === '/';
+  }
+
+  get isHistory() {
+    return this.router.url === '/history';
+  }
 }
