607cdab02811ae576657941f800c6586b5a344bb
Akash Raju M <akashrajum7@gmail.com>
2023-01-25 23:36:56 +0530

    make search components dynamic

--- src/app/shared/input-field/input-field.component.ts
+++ src/app/shared/input-field/input-field.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.css']
+  styleUrls: ['./input-field.component.css'],
 })
 export class InputFieldComponent {
+  @Input()
+  label: string = '';
+  @Input()
+  type: string = 'text';
+  @Input()
+  id: string = '';
+  @Input()
+  placeholder: string = '';
+  @Input()
+  value: string = '';
+  @Input()
+  name: string = '';
+  @Input()
+  required: boolean = false;
+  @Input()
+  disabled: boolean = false;
 
+  @Output() valueChange = new EventEmitter<string>();
 }
--- src/app/shared/primary-button/primary-button.component.ts
+++ src/app/shared/primary-button/primary-button.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
   templateUrl: './primary-button.component.html',
-  styleUrls: ['./primary-button.component.css']
+  styleUrls: ['./primary-button.component.css'],
 })
 export class PrimaryButtonComponent {
-
+  @Input()
+  label: string = '';
+  @Input()
+  type: string = 'submit';
+  @Input()
+  disabled: boolean = false;
 }
