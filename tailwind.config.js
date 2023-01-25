8d3a0d4a57e1bcc6126ca60e8096932e0933524b
Akash Raju M <akashrajum7@gmail.com>
2023-01-25 22:10:03 +0530

    add tailwindcss and basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
