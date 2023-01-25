22fcca7d7ea82f1b6f5143a805e0fd4cf29f1473
Akash Raju M <akashrajum7@gmail.com>
2023-01-25 22:50:36 +0530

    add all basic components for home screen

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
-
+import { InputFieldComponent } from './input-field/input-field.component';
+import { PrimaryButtonComponent } from './primary-button/primary-button.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    InputFieldComponent,
+    PrimaryButtonComponent,
+    UserCardComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [
+    NavBarComponent,
+    InputFieldComponent,
+    PrimaryButtonComponent,
+    UserCardComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
